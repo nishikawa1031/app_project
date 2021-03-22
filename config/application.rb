dc266d5d60065894da4b712dd9729fc587fefec4
nishikawa1031 <tatsunishitatsu@gmail.com>
2021-03-22 22:44:06 +0900

    i18nの設定が完了

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,13 @@ module SampleApp
 
     # 認証トークンをremoteフォームに埋め込む
     config.action_view.embed_authenticity_token_in_remote_forms = true
+
+    # 多言語対応
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja # デフォルトのlocaleを日本語(:ja)にする
+
+    #　#　以下の記述を追記する(設定必須)
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
